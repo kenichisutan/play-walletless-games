780d33e131ba70fe2e83933255f7df9c62804cd4
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2023-02-18 11:41:42 +0900

    Created login form and temp admin login.

--- play-walletless-games-front-end/src/components/Login.js
+++ play-walletless-games-front-end/src/components/Login.js
@@ -1,5 +1,47 @@
+import {useState} from "react";
+import Input from "./form/Input";
+
 const Login = () => {
-    return(<>Login</>)
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(username === "admin" && password === "admin") {
+            alert("Login successful!");
+        }
+    }
+
+    return (
+        <div className="col-md-6 offset-md-3">
+            <h2>Login</h2>
+            <hr/>
+            <form onSubmit={handleSubmit}>
+                <Input
+                    title="Username"
+                    type="username"
+                    className="form-control"
+                    name="username"
+                    autoComplete="username-new"
+                    onChange={(event) => setUsername(event.target.value)}
+                />
+                <Input
+                    title="Password"
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    autoComplete="password-new"
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+                <hr />
+                <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Login"
+                />
+            </form>
+        </div>
+    );
 }
 
 export default Login;
