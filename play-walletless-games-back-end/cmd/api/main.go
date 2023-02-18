faf9c0c101dcd774090d2be494a1f002089c2f1c
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2023-02-18 22:46:43 +0900

    Created backend files using Golang

---
+++ play-walletless-games-back-end/cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	// TODO: Set application configuration
+	var app application
+
+	// TODO: Read from command line arguments
+
+	// TODO: Connect to database
+	app.Domain = "example.com"
+	log.Println("Starting server on port", port, "for domain", app.Domain, "...")
+
+	// TODO: Start a web server
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
