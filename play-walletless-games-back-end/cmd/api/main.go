1dd415b303ab4005d80cc5a9ff6c287785fec499
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2023-02-20 00:22:25 +0900

    Utilizing go-chi to create routes

--- play-walletless-games-back-end/cmd/api/main.go
+++ play-walletless-games-back-end/cmd/api/main.go
@@ -22,10 +22,8 @@ func main() {
 	app.Domain = "example.com"
 	log.Println("Starting server on port", port, "for domain", app.Domain, "...")
 
-	http.HandleFunc("/", Hello)
-
 	// TODO: Start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
--- play-walletless-games-back-end/cmd/api/routes.go
+++ play-walletless-games-back-end/cmd/api/routes.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	// Create a router mux and register the routes.
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Get("/", app.Home)
+
+	return mux
+}
