5666bbb47190121bfdeb29898a2c1be8194e19dc
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2023-02-21 10:29:26 +0900

    Home now displays json

--- play-walletless-games-back-end/cmd/api/handlers.go
+++ play-walletless-games-back-end/cmd/api/handlers.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home, world from %s", app.Domain)
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Welcome to the Walletless Games API",
+		Version: "1.0.0",
+	}
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
