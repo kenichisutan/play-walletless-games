67cd453f83f4b8cba6a3e5aae2f587235390478a
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2023-03-07 19:25:44 +0900

    Added auth functionality

--- play-walletless-games-back-end/cmd/api/main.go
+++ play-walletless-games-back-end/cmd/api/main.go
@@ -7,14 +7,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 const port = 8080
 
 type application struct {
-	DSN    string
-	Domain string
-	DB     repository.DatabaseRepo
+	DSN          string
+	Domain       string
+	DB           repository.DatabaseRepo
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
 }
 
 func main() {
@@ -23,6 +29,11 @@ func main() {
 
 	// Read from command line arguments
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=games sslmode=disable timezone=UTC+9 connect_timeout=5", "Postgres connection string")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "secretsample", "signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "play-walletless-games.com", "signing issuer")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "play-walletless-games.com", "signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "play-walletless-games.com", "domain")
 	flag.Parse()
 
 	// Connect to database
@@ -34,7 +45,17 @@ func main() {
 	// Close the connection when the main() function exits.
 	defer app.DB.Connection().Close()
 
-	app.Domain = "example.com"
+	app.auth = Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudience,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath:    "/",
+		CookieName:    "__Host-refresh_token",
+		CookieDomain:  app.CookieDomain,
+	}
+
 	log.Println("Starting server on port", port, "for domain", app.Domain, "...")
 
 	// Start a web server
