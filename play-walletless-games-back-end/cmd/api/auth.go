e0d5de2af28abb145496af2e0171c599c56f81e0
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2023-03-07 19:10:21 +0900

    Added GenerateTokenPair function

--- play-walletless-games-back-end/cmd/api/auth.go
+++ play-walletless-games-back-end/cmd/api/auth.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/golang-jwt/jwt/v4"
 	"time"
 )
@@ -27,6 +28,57 @@ type jwtClaims struct {
 	RefreshToken string `json:"refresh_token"`
 }
 
+type TokenPairs struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
 type Claim struct {
 	jwt.RegisteredClaims
+}
+
+func (j *Auth) GenerateTokenPair(user *jwtUser) (TokenPairs, error) {
+	// Create a token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set the claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = fmt.Sprintf("%s %s", user.FirstName, user.LastName)
+	claims["sub"] = fmt.Sprint(user.ID)
+	claims["aud"] = j.Audience
+	claims["iss"] = j.Issuer
+	claims["iat"] = time.Now().UTC().Unix()
+	claims["typ"] = "JWT"
+
+	// Set the expiry time for jwt
+	claims["exp"] = time.Now().Add(j.TokenExpiry).UTC().Unix()
+
+	// Create a signed token
+	signedAccessToken, err := token.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+	// Create a refresh token and set claims
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	refreshTokenClaims := refreshToken.Claims.(jwt.MapClaims)
+	refreshTokenClaims["sub"] = fmt.Sprint(user.ID)
+	refreshTokenClaims["iat"] = time.Now().UTC().Unix()
+
+	// Set the expiry time for refresh token
+	refreshTokenClaims["exp"] = time.Now().Add(j.RefreshExpiry).UTC().Unix()
+
+	// Create signed refresh token
+	signedRefreshToken, err := refreshToken.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	// Create TokenPairs and populate with tokens
+	var tokenPairs = TokenPairs{
+		AccessToken:  signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}
+
+	// Return TokenPairs
+	return tokenPairs, nil
 }
